07a02e25cb2d4205d1f4fe80861e3d285765a744
Dennis van Schie <1998dennis@live.nl>
2019-05-21 12:00:18 +0200

    Minor vehicle specification additions to the show listing blade

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -42,6 +42,22 @@
 			<hr>
 			<h5>Merk: {{$listing['vehicle']->make}}</h5>
 			<h5>Model: {{$listing['vehicle']->model}}</h5>
+			<h5>Km stand: {{$listing['vehicle']->mileage}}</h5>
+			<h5>Kenteken: {{$listing['vehicle']->license_plate}}</h5>
+			<h5>Kleur: {{$listing['vehicle']->color}}</h5>
+			<h5>APK vervaldatum: {{$listing['vehicle']->apk_expiration}}</h5>
+			<h5>Versnellingsbak: {{$listing['vehicle']->transmission}}</h5>
+			<h5>Versnellingen: {{$listing['vehicle']->gears}}</h5>
+			<h5>Motor inhoud: {{$listing['vehicle']->engine_capicity}}</h5>
+			<h5>Aantal cilinders: {{$listing['vehicle']->cylinders}}</h5>
+			<h5>Leeg gewicht: {{$listing['vehicle']->empty_weight}}</h5>
+			<h5>Aandrijving: {{$listing['vehicle']->drive}}</h5>
+			<h5>Brandstof: {{$listing['vehicle']->fuel_type}}</h5>
+			<h5>Aantal deuren: {{$listing['vehicle']->doors}}</h5>
+			<h5>Aantal zitplaatsen: {{$listing['vehicle']->seats}}</h5>
+			<h5>Vermogen: {{$listing['vehicle']->power}}</h5>
+
+			
 		</div>
 
 	</div>
